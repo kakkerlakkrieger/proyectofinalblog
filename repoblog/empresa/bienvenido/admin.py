7422d80b5102c548db7d7193711c3a506288dd3f
kakkerlakkrieger <532mhz@gmail.com>
2021-12-20 03:03:34 -0300

    eliminando carpetas innecesarias

--- repoblog/empresa/bienvenido/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from bienvenido.models import Departamento, Empleado
-# Register your models here.
-
-admin.site.register (Departamento)
-admin.site.register (Empleado)
